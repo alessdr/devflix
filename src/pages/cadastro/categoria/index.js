11b95fc0e3cd8f5dad15bdd70f0a03e2e12e0382
Alessandro Rocha <alessdr@gmail.com>
2020-07-31 15:21:37 -0300

    Imerssao React Alura - Aula 4

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,91 +1,107 @@
-import React, { useState } from 'react';
-import PageTemplate from '../../../components/PageTemplate'
-import FormField from '../../../components/FormField'
-import FormTextArea from '../../../components/FormTextArea'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageTemplate from '../../../components/PageTemplate';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
+  /* Const para "definir" a estrutura de dados */
+  const itemCategoria = {
+    nome: '',
+    descricao: '',
+    cor: '#ffffff',
+  };
 
-    /* Const para "definir" a estrutura de dados */
-    const itemCategoria = {
-        nome: '',
-        descricao: '',
-        cor: ''
-    }
-    
-    /* Cria o state 'listaCategorias' e a funcao que responsavel por setar seu valor, atribuindo um [] (Array vazio) */
-    const [listaCategorias, setListaCategorias] = useState([])
-    
-    /* Cria o state dados e a funcao que responsavel por setar seu valor, atribuindo a const itemCategoria */
-    const [atributos, setAtributos] = useState(itemCategoria);
-    
+  /* Cria o state 'listaCategorias' e a funcao responsavel p/ setar seu valor, atribuindo um [] */
+  const [listaCategorias, setListaCategorias] = useState([]);
 
-    /* Funcao para atualizar a estrutura de dados */
-    function setAtributo(chave, valor) {
-        setAtributos({
-            ...atributos,
-            [chave]: valor
-        })
-    }
+  /* Cria o state dados e a funcao responsavel p/ setar seu valor, atribuindo itemCategoria */
+  const [atributos, setAtributos] = useState(itemCategoria);
 
-    //Funcao generica utilizada p/ atualizar a estrutura de dados e p/ consequencia os input's, c/ base no atributo "name"
-    function updateCategoriaHandler(event) {
-        setAtributo(
-            event.target.getAttribute('name'), 
-            event.target.value
-            );
-            
-        /*
+  /* Funcao para atualizar a estrutura de dados */
+  function setAtributo(chave, valor) {
+    setAtributos({
+      ...atributos,
+      [chave]: valor,
+    });
+  }
+
+  // Funcao generica utilizada p/ atualizar a estrutura de dados e p/ consequencia os input's,
+  // com base no atributo "name"
+  function updateCategoriaHandler(event) {
+    setAtributo(
+      event.target.getAttribute('name'),
+      event.target.value,
+    );
+
+    /*
         const { getAttribute, value } = event.target;
         setAtributo(
-            getAttribute('name'), 
+            getAttribute('name'),
             value
         );
-        */
-    }
+    */
+  }
 
-    /* Funcao para interceptar o "submit" e executar alguma acao */
-    function handleSubmit(event){
-        /* Para a propagacao do evento */
-        event.preventDefault();
-        /* Seta as categorias com a lista ja existente '...' e mais o conteudo da var dados */
-        setListaCategorias([ ...listaCategorias, atributos]);
-        /* limpar os input's */
-        setAtributos(itemCategoria)
-    } 
+  /* Funcao para interceptar o "submit" e executar alguma acao */
+  function handleSubmit(event) {
+    /* Para a propagacao do evento */
+    event.preventDefault();
+    /* Seta as categorias com a lista ja existente '...' e mais o conteudo da var dados */
+    setListaCategorias([...listaCategorias, atributos]);
+    /* limpar os input's */
+    setAtributos(itemCategoria);
+  }
 
-    return (
-        <PageTemplate>
-            <h1>Cadastro de Categoria: {atributos.nome}</h1>
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL).then(async (response) => {
+      const resposta = await response.json();
+      setListaCategorias(resposta);
+    });
+  }, []);
 
-            <form onSubmit={handleSubmit}>
-                
-                {/* Componentes personalizados */}
-                <FormField label="Categoria" type="text" value={atributos.nome} name="nome" onChange={updateCategoriaHandler}/>
-                
-                <FormTextArea label="Descrição" value={atributos.descricao} name="descricao" onChange={updateCategoriaHandler}/>
+  return (
+    <PageTemplate>
+      <h1>
+        Cadastro de Categoria:
+        {atributos.nome}
+      </h1>
 
-                <FormField label="Cor" type="color" value={atributos.cor} name="cor" onChange={updateCategoriaHandler}/>
+      <form onSubmit={handleSubmit}>
 
-                <button>Cadastrar</button>
+        {/* Componentes personalizados */}
+        <FormField label="Categoria" type="text" value={atributos.nome} name="nome" onChange={updateCategoriaHandler} />
 
-            </form>
+        <FormField label="Descrição" type="textarea" value={atributos.descricao} name="descricao" onChange={updateCategoriaHandler} />
 
-            <ul>
-                {listaCategorias.map( (categoria, index) => {
-                    return (
-                        <li key={`${categoria}${index}`}> {/* concatena categoria com index */}
-                            {categoria.nome}
-                        </li>
-                    )
-                } )}
-            </ul>
+        <FormField label="Cor" type="color" value={atributos.cor} name="cor" onChange={updateCategoriaHandler} />
 
-            <Link to="/">
-                Ir para home
-            </Link>
-        </PageTemplate>
-    )
+        <Button>
+          Cadastrar
+        </Button>
+
+      </form>
+
+      {listaCategorias.length === 0 && (
+      <div>
+        Loading...
+      </div>
+      )}
+
+      <ul>
+        {listaCategorias.map((categoria) => (
+          <li key={`${categoria.id}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageTemplate>
+  );
 }
 
 export default CadastroCategoria;
